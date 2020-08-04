e27c86d919871f3b2e9f6018739a11c9f0ef6da2
조경진 <kj_cho@idealidea.co.kr>
2020-08-04 19:37:41 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/uipractice_20200804/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.uipractice_20200804
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
